f81f968a8460941e5b55afe3b43452947bddaacb
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-08-15 11:32:21 -0700

    react first app

---
+++ src/react-first-app/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <HelloWorld />
+        <App />
+    </React.StrictMode>
+);
+console.log('hello react world');
+
+function HelloWorld() {
+    console.log('hello world fund');
+    return <h2>Hello World!!!</h2>;
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
