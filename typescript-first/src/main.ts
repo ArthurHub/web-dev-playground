7b8663bc2cc097d4810698e3cdc775ef6760dc78
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-08-19 16:26:30 -0700

    misc horseplay

--- typescript-first/src/main.ts
+++ typescript-first/src/main.ts
@@ -1,14 +1,31 @@
+import * as fs from "fs";
 import { doSomething } from "./play1.js";
 import * as play2 from "./play2.js";
 
-function main() {
+async function main() {
   // hello world
   var message = "Hello World";
   console.log(message);
 
+  await readFiles();
+
   doSomething();
 
   play2.doSomething();
+}
+
+// read all the files in current folder
+async function readFiles() {
+  try {
+    const files = await fs.promises.readdir(".");
+    console.debug("Files:");
+    for (const file of files) {
+      const stats = await fs.promises.stat(file);
+      console.debug(`${stats.isFile() ? "File" : "Folder"}: ${file};`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 main();
