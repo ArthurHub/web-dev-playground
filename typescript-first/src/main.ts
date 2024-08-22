4f3a942c014f915350dd024cdb9ed1ba47d8852c
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-08-22 14:39:44 -0700

    ts learning

--- typescript-first/src/main.ts
+++ typescript-first/src/main.ts
@@ -1,11 +1,21 @@
-import * as fs from "fs";
-import { doSomething } from "./play1.js";
-import * as play2 from "./play2.js";
+import * as fs from 'fs';
+import { doSomething } from './play1.js';
+import * as play2 from './play2.js';
+
+type SomeType = string | number | any;
 
 async function main() {
   // hello world
-  var message = "Hello World";
+  var message = 'Hello World';
   console.log(message);
+
+  displayValue('hello');
+  displayValue(34534);
+  displayValue(false);
+  displayValue(null);
+  let bla;
+  displayValue(bla);
+  displayValue(() => {});
 
   await readFiles();
 
@@ -14,14 +24,21 @@ async function main() {
   play2.doSomething();
 }
 
+function displayValue(value: SomeType) {
+  console.log(`The typeof of value is: ${typeof value}`);
+  if (typeof value === 'string') {
+    console.log(`The value is a string: "${value}"`);
+  }
+}
+
 // read all the files in current folder
 async function readFiles() {
   try {
-    const files = await fs.promises.readdir(".");
-    console.debug("Files:");
+    const files = await fs.promises.readdir('.');
+    console.debug('Files:');
     for (const file of files) {
       const stats = await fs.promises.stat(file);
-      console.debug(`${stats.isFile() ? "File" : "Folder"}: ${file};`);
+      console.debug(`${stats.isFile() ? 'File' : 'Folder'}: ${file};`);
     }
   } catch (err) {
     console.error(err);
