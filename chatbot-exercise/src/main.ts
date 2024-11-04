a4222257588fc61842b1825f422c54c4c89c1da7
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-11-04 15:27:40 -0800

    chatbot - patient data request

--- chatbot-exercise/src/main.ts
+++ chatbot-exercise/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import { HealthAssistant } from './health-assistant.js';
+
+async function main(): Promise<void> {
+  const assistant = new HealthAssistant();
+
+  console.log('\n---\n');
+
+  await assistant.getPatientData();
+
+  console.log('\n\n---\n');
+}
+
+await main();
