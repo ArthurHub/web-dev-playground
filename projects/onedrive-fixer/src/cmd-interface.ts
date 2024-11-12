b157d51f313e86856af02f4fffb6d25f121f43ca
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-11-12 13:59:06 -0800

    onedrive fixer: add cache data of last used paths to cmd user interface

--- projects/onedrive-fixer/src/cmd-interface.ts
+++ projects/onedrive-fixer/src/cmd-interface.ts
@@ -16,6 +16,8 @@ import { getLogger } from 'common/logger.js';
 import * as fs from 'fs';
 import { OneDriveNameDateFixer } from './onedrive-name-date-fixer.js';
 import { OneDriveFixedFileStatus, type OneDriveFixedFile } from './entities.js';
+import path from 'path';
+import { tmpdir } from 'os';
 
 const logger = getLogger('cmd-interface');
 
@@ -24,6 +26,19 @@ enum Options {
   ConvertHEICtoJPEG,
 }
 
+/** Simple cache of last used paths */
+interface OneDriveFixerData {
+  lastUsedPaths: string[];
+}
+
+/** Use temp dir to store cache data file */
+const onedriveFixerDataFile = path.join(tmpdir(), 'onedrive-fixer-data.json');
+
+/**
+ * Run a command line user interface to select an option for OneDrive fixer.
+ * Ask the user for the folder path to operate on.
+ * Use a cache to store the last used paths and present select if available.
+ */
 export async function runOneDriveFixerCmdUserInterface() {
   try {
     // Step 1: Select an option
@@ -35,22 +50,41 @@ export async function runOneDriveFixerCmdUserInterface() {
       ],
     });
 
-    // Step 2: Ask for file path
-    const filePath = await input({
-      message: 'Enter the folder path to operate on:',
-      validate: (inputValue: string) => {
-        return fs.existsSync(inputValue) ? true : 'The specified file path does not exist.';
-      },
-    });
+    let folderPath = '';
+
+    // read cache data and ask for a previously used path
+    const data = readData();
+    if (data.lastUsedPaths.length) {
+      const choices = data.lastUsedPaths.map((path) => ({ name: path, value: path }));
+      choices.push({ name: 'Other', value: '' });
+      folderPath = await select({
+        message: 'Select a previously used path:',
+        choices,
+      });
+    }
+
+    // check if we have an existing folder
+    if (!folderPath || !fs.existsSync(folderPath)) {
+      // Ask for file path
+      folderPath = await input({
+        message: 'Enter the folder path to operate on:',
+        validate: (inputValue: string) => {
+          return fs.existsSync(inputValue) ? true : 'The specified file path does not exist.';
+        },
+      });
+    }
+
+    // write the new path to the data
+    writeData(data, folderPath);
 
     // Ask if dry run
     const isDryRun = await confirm({ message: 'Dry-run?', default: true });
 
     // Step 3: Perform action based on selected option
     if (option === Options.FixFileNames) {
-      await runOneDriveNameDateFixer(filePath, isDryRun);
+      await runOneDriveNameDateFixer(folderPath, isDryRun);
     } else {
-      logger.info(`Performing Option B on file: ${filePath}`);
+      logger.info(`Performing Option B on file: ${folderPath}`);
       // Add the logic for Option B here
     }
   } catch (error) {
@@ -65,7 +99,15 @@ async function runOneDriveNameDateFixer(filePath: string, isDryRun: boolean): Pr
 
     const { updated, noUpdateRequired, skippedNotIPhone } = getCounts(handledFiles);
     logger.info(
-      'Finished running OneDrive fixer handling %d files: %d updated, %d no update required, %d not iPhone, %d other',
+      `
+-------------
+Finished running OneDrive media file name/date fixer: 
+%d media files processed
+%d updated
+%d no update required
+%d not iPhone
+%d other
+-------------`,
       handledFiles.length,
       updated,
       noUpdateRequired,
@@ -95,4 +137,21 @@ function getCounts(handledFiles: OneDriveFixedFile[]): {
     }
   }
   return { updated, noUpdateRequired, skippedNotIPhone };
+}
+
+// Function to read data, typed with OneDriveFixerData
+function readData(): OneDriveFixerData {
+  if (!fs.existsSync(onedriveFixerDataFile)) {
+    return { lastUsedPaths: [] };
+  }
+  const data = fs.readFileSync(onedriveFixerDataFile, 'utf-8');
+  return JSON.parse(data) as OneDriveFixerData;
+}
+
+// Function to write data, ensuring we use OneDriveFixerData structure
+function writeData(data: OneDriveFixerData, folderPath: string): void {
+  data.lastUsedPaths.unshift(folderPath);
+  data.lastUsedPaths = [...new Set(data.lastUsedPaths)];
+  data.lastUsedPaths = data.lastUsedPaths.slice(0, 3);
+  fs.writeFileSync(onedriveFixerDataFile, JSON.stringify(data, null, 2));
 }
