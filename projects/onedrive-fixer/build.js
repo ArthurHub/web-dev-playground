b3c22675ce8cf2169b9c37267d66c99809c76d17
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-11-13 13:49:07 -0800

    onedrive fixer: add bundler and packager (no working)

---
+++ projects/onedrive-fixer/build.js
@@ -0,0 +1,25 @@
+import { build } from 'esbuild';
+
+const ESM_REQUIRE_SHIM = `
+const { dirname } = await import("path");
+const { fileURLToPath } = await import("url");
+globalThis.__filename = fileURLToPath(import.meta.url);
+globalThis.__dirname = dirname(globalThis.__filename);
+import { createRequire } from 'module'; 
+const require = createRequire(import.meta.url);
+`;
+
+const buildOptions = {
+  bundle: true,
+  entryPoints: ['src/main.ts'],
+  outfile: 'dist/bundle/bundle.js',
+  platform: 'node',
+  target: 'node22',
+  format: 'esm',
+  minify: false,
+  treeShaking: false,
+  banner: { js: ESM_REQUIRE_SHIM },
+  external: ['pino'],
+};
+
+await build(buildOptions);
