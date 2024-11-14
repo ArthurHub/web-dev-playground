26ee2135419971202e5b9c3fbf1ac007d16c6e40
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-11-13 18:01:13 -0800

    node-runner: embed the node app we want to run inside the packaged node runner, extract, and run it

--- projects/node-runner/runner.js
+++ projects/node-runner/runner.js
@@ -1,8 +1,34 @@
 const { spawn } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const zlib = require('unzipper');
 
-const child = spawn('cmd.exe', ['/c', 'bin\\node.exe', 'bin\\bundle.mjs'], {
-  detached: true,
-  stdio: 'ignore',
-});
+async function main() {
+  const outDir = path.join(os.tmpdir(), 'node-runner');
+  if (!fs.existsSync(outDir)) {
+    // create temp directory to unzip the bundle
+    fs.mkdirSync(outDir, { recursive: true });
 
-child.unref();
+    // get the bundle zip from the assets folder
+    const bundleZip = path.join(__dirname, './assets/bundle.zip');
+
+    // unzip the bundle into the temp directory
+    await fs
+      .createReadStream(bundleZip)
+      .pipe(zlib.Extract({ path: outDir }))
+      .promise();
+  }
+
+  // run the bundle with the node inside of it in the temp directory
+  const node = path.join(outDir, 'node.exe');
+  const bundle = path.join(outDir, 'bundle.mjs');
+  const child = spawn('cmd.exe', ['/c', node, bundle], {
+    detached: true,
+    stdio: 'ignore',
+  });
+
+  child.unref();
+}
+
+main();
