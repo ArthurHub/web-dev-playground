34b1c67d6efbb1b1d56529c53123a3fbc6c39845
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-11-18 11:19:39 -0800

    misc

---
+++ projects/typescript-learn/src/play8.ts
@@ -0,0 +1,27 @@
+// Therefore those skilled at the unorthodox
+// are infinite as heaven and earth,
+// inexhaustible as the great rivers.
+// When they come to an end,
+// they begin again,
+// like the days and months;
+// they die and are reborn,
+// like the four seasons.
+//
+// - Sun Tsu, The Art of War.
+//
+// ArthurHub, 2024
+
+class Foo {
+  public bla?: string = 'sdf';
+
+  public func() {
+    this.bla = undefined;
+    console.log(bla);
+
+    const num = '5' == 2;
+  }
+}
+
+let bla: string;
+
+console.log(bla);
